c2ed125aa3f6dbb8587d3a36b0cfcb6dd9873a2e
SarthakChaudhari-2905 <chaudharisarthak295@gmail.com>
2026-05-17 00:51:55 +0530

    Updated frontend API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL:
+    "https://gigflow-smart-dashboard-1.onrender.com/api",
 });
 
 export default API;
